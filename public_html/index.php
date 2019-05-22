5a6cc0647212a717df931f514ac64f3e161cf249
Adam Gardner <adam.gardner@dynatrace.com>
2019-05-22 18:20:10 +1000

    Create index.php

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<?
+  $pause = 1;
+  sleep($pause);
+?>
+<!doctype html>
+<html>
+<head>
+<title>Dummy PHP v1</title>
+</head>
+<body>
+<h1>Dummy PHP v1</h1>
+<p>Wait is <?php echo $pause; ?> second.</p>
+</body>
+</html>
